5e77eb086200d1d6a19470e201916619cf4b9c62
paul <alettertopaulrobinson0099@gmail.com>
2023-08-08 11:33:01 +0300

    Create my cocktail blank fragment view

---
+++ app/src/main/java/surf/summerschool23/cocktailbar/MyCocktailsBlankFragment.kt
@@ -0,0 +1,17 @@
+package surf.summerschool23.cocktailbar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class MyCocktailsBlankFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_cocktails_blank, container, false)
+    }
+
+}
