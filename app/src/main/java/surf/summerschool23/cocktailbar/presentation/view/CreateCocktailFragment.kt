4287c96a9b222a308d5f3decfa475c472539363d
paul <alettertopaulrobinson0099@gmail.com>
2023-08-08 19:43:29 +0300

    Add image picker

--- app/src/main/java/surf/summerschool23/cocktailbar/presentation/view/CreateCocktailFragment.kt
+++ app/src/main/java/surf/summerschool23/cocktailbar/presentation/view/CreateCocktailFragment.kt
@@ -1,13 +1,19 @@
 package surf.summerschool23.cocktailbar.presentation.view
 
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.drawable.toBitmap
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import surf.summerschool23.cocktailbar.R
 import surf.summerschool23.cocktailbar.databinding.FragmentCreateCocktailBinding
 import surf.summerschool23.cocktailbar.domain.entity.CocktailEntity
@@ -17,7 +23,9 @@ import surf.summerschool23.cocktailbar.presentation.viewmodel.CreateCocktailView
 class CreateCocktailFragment : Fragment() {
 
     private lateinit var binding: FragmentCreateCocktailBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     private val viewModel by viewModel<CreateCocktailViewModel>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,26 +34,8 @@ class CreateCocktailFragment : Fragment() {
 
         binding.createCocktailAddPhotoIv.clipToOutline = true
 
-        binding.createCocktailSaveBtn.setOnClickListener{
-            if (validateFields()) {
-                val cocktailEntity = CocktailEntity(
-                    title = binding.createCocktailTitleEt.text.toString(),
-                    description = binding.createCocktailDescriptionEt.text.toString(),
-                    recipe = binding.createCocktailRecipeEt.text.toString(),
-                    image = BitmapConverter().toByteArray(
-                        BitmapFactory.decodeResource(requireContext().resources,
-                        R.drawable.cocktail_placeholder)
-                    ),
-                    ingredients = ArrayList()
-                )
-                viewModel.saveNewCocktail(cocktailEntity)
-                findNavController().navigate(R.id.action_createCocktailFragment_to_myCocktailsFragment)
-            }
-        }
-
-        binding.createCocktailCancelBtn.setOnClickListener{
-            findNavController().navigateUp()
-        }
+        registerResult()
+        setListeners()
 
         return binding.root
     }
@@ -58,5 +48,53 @@ class CreateCocktailFragment : Fragment() {
         }
         return true
     }
+
+    private fun setListeners() {
+        binding.createCocktailSaveBtn.setOnClickListener{
+            if (validateFields()) {
+                val cocktailEntity = CocktailEntity(
+                    title = binding.createCocktailTitleEt.text.toString(),
+                    description = binding.createCocktailDescriptionEt.text.toString(),
+                    recipe = binding.createCocktailRecipeEt.text.toString(),
+                    image = BitmapConverter().toByteArray(
+                        binding.createCocktailAddPhotoIv.drawable.toBitmap()
+                    ),
+                    ingredients = ArrayList()
+                )
+                viewModel.saveNewCocktail(cocktailEntity)
+                findNavController().navigate(R.id.action_createCocktailFragment_to_myCocktailsFragment)
+            }
+        }
+
+        binding.createCocktailCancelBtn.setOnClickListener{
+            findNavController().navigateUp()
+        }
+
+        binding.createCocktailAddPhotoIv.setOnClickListener {
+            pickImage()
+        }
+    }
+
+    private fun pickImage() {
+        //val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        resultLauncher.launch(intent)
+    }
+
+    private fun registerResult() {
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            try {
+                val uri: Uri? = result.data?.data
+                binding.createCocktailAddPhotoIv.setImageURI(uri)
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), "No image was picked", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
 
 }
