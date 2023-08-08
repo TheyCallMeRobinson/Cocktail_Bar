14aa439afdb681b11d9172ad4b75600b5e4dcc83
paul <alettertopaulrobinson0099@gmail.com>
2023-08-08 10:37:04 +0300

    Init commit

---
+++ app/src/main/java/surf/summerschool23/cocktailbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package surf.summerschool23.cocktailbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
